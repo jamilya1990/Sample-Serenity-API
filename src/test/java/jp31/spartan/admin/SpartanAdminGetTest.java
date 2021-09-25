bc36e080a3759a6ab1e8e84b1759328711e259d9
Jamilya Petsenyuk <jamilya.petsenyuk@gmail.com>
2021-09-24 22:18:45 -0700

    Commit 1

--- src/test/java/jp31/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/jp31/spartan/admin/SpartanAdminGetTest.java
@@ -1,4 +1,26 @@
 package jp31.spartan.admin;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
 public class SpartanAdminGetTest {
+
+    @BeforeAll
+    public static void init(){
+        baseURI = "http://44.195.19.167:7000";
+    }
+
+    @Test
+    public void getAllSpartans(){
+        given()
+                .accept(ContentType.JSON)
+                .auth().basic("admin","admin")
+         .when().get("api/spartans")
+         .then().statusCode(200)
+                .contentType(ContentType.JSON);
+    }
 }
